e13237fdd4cc3c2732feb0efb9d69c4896f2f0ae
Kaiki Mello dos Santos <kaiki.mello@gmail.com>
2023-08-17 17:57:48 -0300

    Criando arquivo de .gitignore e tipos_de_dado.py

---
+++ conhecendo-linguagem/tipos_de_dado.py
@@ -0,0 +1,14 @@
+# O tipo informa ao interpetretador quanto será gasto de memória
+
+print(11 + 10 + 1000)
+print(1.5 + 1 + 0.5)
+print(True)
+print(False)
+print("Python")
+
+# São mantidos pelas respectivas classes
+
+int()
+float()
+str()
+bool()
