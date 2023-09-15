99330f5c4ff46b353b57c103b06c5be1f4a9b828
Kaiki Mello <kaiki.mello@gmail.com>
2023-09-15 15:33:13 -0300

    Finalizando aula sobre conversão de tipos

---
+++ conhecendo-linguagem/convertendo_tipos.py
@@ -0,0 +1,13 @@
+print(int(1.0))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2)
+# Só retorna a parte inteira e ignora a parte flutuante
+print(100 // 2)
